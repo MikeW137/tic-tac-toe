878da432d6eac62f4b72c28ab7c73e2d7f673793
Michael Wydra <wydrama@gmail.com>
2021-03-17 14:27:24 -0500

    added restart game

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,40 @@
 let currentPlayer = 1;
 const boxes = document.querySelectorAll(".box")
-boxes.forEach(box => {
-    box.addEventListener('click', clicked, {once: true})
-    box.addEventListener('mouseover', hover)
-    box.addEventListener('mouseout', removeHover)
-})
+document.getElementById("resetButton").addEventListener('click', resetBoard);
+
+function startGame() {
+    boxes.forEach(box => {
+        box.addEventListener('click', clicked, {once: true})
+        box.addEventListener('mouseover', hover)
+        box.addEventListener('mouseout', removeHover)
+    })
+    currentPlayer = 1;
+}
+startGame();
 
 function hover() {
     if (currentPlayer) {
-        this.classList.add('redhover')
+        this.classList.add('blackhover')
     } else {
-        this.classList.add('hoverblue')
+        this.classList.add('hoverwhite')
     }
 }
 
 function removeHover() {
-    this.classList.remove('redhover')
-    this.classList.remove('hoverblue')
+    this.classList.remove('blackhover')
+    this.classList.remove('hoverwhite')
 }
 
 
 function clicked() {
     if (currentPlayer) {
-        this.classList.add('red')
+        this.classList.add('black')
+        this.removeEventListener('mouseover', hover)
         currentPlayer = 0;
 
     } else {
-        this.classList.add('blue')
+        this.classList.add('white')
+        this.removeEventListener('mouseover', hover)
         currentPlayer = 1;
     }
     checkWinner()
@@ -34,8 +42,24 @@ function clicked() {
 
 
 function checkWinner() {
-    if (boxes[0].classList.contains("blue") && boxes[1].classList.contains("blue") && boxes[2].classList.contains("blue")) {
+    if (boxes[0].classList.contains("white") && boxes[1].classList.contains("white") && boxes[2].classList.contains("white")) {
         boxes.forEach(box => {box.removeEventListener('click', clicked)})
-
+        presentWinner("white wins!")
     }
+}
+
+
+function presentWinner(color) {
+    const resultArea = document.querySelector(".result");
+    resultArea.innerHTML = "Result: " + color;
+
+}
+
+function resetBoard() {
+    boxes.forEach(box => {
+        box.classList.remove('white');
+        box.classList.remove('black');
+    })
+
+    startGame();
 }
