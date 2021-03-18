b99278958b0a0a7fbaafa7e40c6fa613eccad0ec
Michael Wydra <wydrama@gmail.com>
2021-03-18 12:26:19 -0500

    added win counters

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ let moves = new Array(9);
 const boxes = document.querySelectorAll(".box")
 const resultArea = document.querySelector(".result");
 document.getElementById("resetButton").addEventListener('click', resetBoard);
+let xWinCount = 0;
+let oWinCount = 0;
+let drawCount = 0;
 
 function startGame() {
     boxes.forEach(box => {
@@ -31,8 +34,11 @@ function removeHover() {
 
 
 function clicked(event) {
+    let x = document.createElement('img');
+    x.setAttribute('src', "images/X.svg");
 
     if (turnCount % 2 == 0) {
+        // this.appendChild(x)
         this.classList.add('black')
         turn = 'O';
     } else {
@@ -51,40 +57,40 @@ function clicked(event) {
 function checkWinner() {
     if (moves[0] !== undefined && moves[0] === moves[1] && moves[1] === moves[2]) {
         presentWinner(moves[0])
-        console.log('first condition')
      } else if (moves[3] !== undefined && moves[3] === moves[4] && moves[4] === moves[5]) {
         presentWinner(moves[3])
-        console.log('second condition')
     } else if (moves[6] !== undefined && moves[6] === moves[7] && moves[7] === moves[8]) {
         presentWinner(moves[6])
-        console.log('third condition')
     } else if (moves[0] !== undefined && moves[0] === moves[3] && moves[3] === moves[6]) {
         presentWinner(moves[3])
-        console.log('fourth')
     } else if (moves[1] !== undefined && moves[1] === moves[4] && moves[4] === moves[7]) {
         presentWinner(moves[1])
-        console.log('fifth')
     } else if (moves[2] !== undefined && moves[2] === moves[5] && moves[5] === moves[8]) {
         presentWinner(moves[2])
-        console.log('sixth')
     } else if (moves[0] === moves[4] && moves[4] === moves[8]) {
         presentWinner(moves[0])
     } else if (moves[2] === moves[4] && moves[4] === moves[6]) {
         presentWinner(moves[2])
     } else if (turnCount == 9) {
         resultArea.innerHTML = "Result: draw";
+        drawCount++;
+        document.querySelector(".draws").innerHTML = drawCount
+
     }
 }
 
 
 function presentWinner(player) {
-    console.log(player)
-    console.log(player == 'O')
     if (player == 'O') {
         resultArea.innerHTML = "Result: black wins";
+        oWinCount++;
     } else {
         resultArea.innerHTML = "Result: white wins";
+        xWinCount++
     }
+    console.log(oWinCount)
+    document.querySelector(".p1-wins").innerHTML = oWinCount
+    document.querySelector(".p2-wins").innerHTML = xWinCount
     boxes.forEach(box => {box.removeEventListener('click', clicked)})
 }
 
